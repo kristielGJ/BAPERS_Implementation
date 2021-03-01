3b198754c3b6df2d0c643af8f7cc7e06d6d30530
mk7531 <61507956+mk7531@users.noreply.github.com>
2021-03-01 19:50:19 +0000

    Add files via upload

---
+++ generated-code/customers/ValuedCustomer.java
@@ -0,0 +1,38 @@
+package Customers;
+
+public class ValuedCustomer extends Customer {
+
+	private Date paymentDeadline;
+	private String discount_plan;
+
+	/**
+	 * 
+	 * @param paymentDeadline
+	 */
+	public void setPaymentDeadline(Date paymentDeadline) {
+		this.paymentDeadline = paymentDeadline;
+	}
+
+	public Date getPaymentDeadline() {
+		return this.paymentDeadline;
+	}
+
+	/**
+	 * 
+	 * @param discount_plan
+	 */
+	public void setDiscount_plan(int discount_plan) {
+		// TODO - implement ValuedCustomer.setDiscount_plan
+		throw new UnsupportedOperationException();
+	}
+
+	public String getDiscount_plan() {
+		return this.discount_plan;
+	}
+
+	public ValuedCustomer() {
+		// TODO - implement ValuedCustomer.ValuedCustomer
+		throw new UnsupportedOperationException();
+	}
+
+}
