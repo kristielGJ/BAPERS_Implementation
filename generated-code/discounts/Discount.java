21510034f8a8a0d02d259e7536ce1c085973aa18
mk7531 <61507956+mk7531@users.noreply.github.com>
2021-03-01 19:53:01 +0000

    Add files via upload

---
+++ generated-code/discounts/Discount.java
@@ -0,0 +1,21 @@
+package Discounts;
+
+public class Discount {
+
+	private double sub_price;
+	private double discount_rate;
+
+	public Discount() {
+		// TODO - implement Discount.Discount
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param discount_rate
+	 */
+	public double getDiscount_rate(double discount_rate) {
+		return this.discount_rate;
+	}
+
+}
---
+++ generated-code/discounts/FlexibleDiscount.java
@@ -0,0 +1,44 @@
+package Discounts;
+
+public class FlexibleDiscount extends Discount {
+
+	private String discount_bands;
+
+	/**
+	 * 
+	 * @param discount_rate
+	 */
+	public float calculatePrice(double discount_rate) {
+		// TODO - implement FlexibleDiscount.calculatePrice
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param discount_bands
+	 * @param discount_rate
+	 */
+	public void setDiscount_rate(String discount_bands, double discount_rate) {
+		// TODO - implement FlexibleDiscount.setDiscount_rate
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param discount_bands
+	 */
+	public void setDiscount_bands(String discount_bands) {
+		this.discount_bands = discount_bands;
+	}
+
+	public double getDiscount_bands() {
+		// TODO - implement FlexibleDiscount.getDiscount_bands
+		throw new UnsupportedOperationException();
+	}
+
+	public FlexibleDiscount() {
+		// TODO - implement FlexibleDiscount.FlexibleDiscount
+		throw new UnsupportedOperationException();
+	}
+
+}
