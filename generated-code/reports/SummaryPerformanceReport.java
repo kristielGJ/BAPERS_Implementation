4721e605223fd29f5da2b59692a5f954ce69f98a
mk7531 <61507956+mk7531@users.noreply.github.com>
2021-03-01 22:42:26 +0000

    reports package

---
+++ generated-code/reports/SummaryPerformanceReport.java
@@ -0,0 +1,20 @@
+package Reports;
+
+public class SummaryPerformanceReport extends Reports {
+
+	/**
+	 * 
+	 * @param from_date
+	 * @param to_date
+	 */
+	public void generateReport(Date from_date, Date to_date) {
+		// TODO - implement SummaryPerformanceReport.generateReport
+		throw new UnsupportedOperationException();
+	}
+
+	public SummaryPerformanceReport() {
+		// TODO - implement SummaryPerformanceReport.SummaryPerformanceReport
+		throw new UnsupportedOperationException();
+	}
+
+}
