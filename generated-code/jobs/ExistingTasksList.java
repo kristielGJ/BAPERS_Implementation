6f762f6a07945a17f7952586e0a87476e7645e30
mk7531 <61507956+mk7531@users.noreply.github.com>
2021-03-01 19:55:11 +0000

    Add files via upload

---
+++ generated-code/jobs/ExistingTasksList.java
@@ -0,0 +1,44 @@
+package Jobs;
+
+public class ExistingTasksList {
+
+	private int numOfExistingTasks = 30;
+
+	/**
+	 * 
+	 * @param task_data
+	 */
+	public boolean extendTaskList(String task_data) {
+		// TODO - implement ExistingTasksList.extendTaskList
+		throw new UnsupportedOperationException();
+	}
+
+	public String[] retrieveExistingTask() {
+		// TODO - implement ExistingTasksList.retrieveExistingTask
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param existing_task_ID
+	 */
+	public void removeExistingTask(int existing_task_ID) {
+		// TODO - implement ExistingTasksList.removeExistingTask
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param existing_task_ID
+	 */
+	public void updateExistingTask(int existing_task_ID) {
+		// TODO - implement ExistingTasksList.updateExistingTask
+		throw new UnsupportedOperationException();
+	}
+
+	public ExistingTasksList() {
+		// TODO - implement ExistingTasksList.ExistingTasksList
+		throw new UnsupportedOperationException();
+	}
+
+}
