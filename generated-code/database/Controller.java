9cf2ea52f98e15492f8f69b2c3318a5e7cf6b724
mk7531 <61507956+mk7531@users.noreply.github.com>
2021-03-01 22:40:16 +0000

    database package

---
+++ generated-code/database/Controller.java
@@ -0,0 +1,172 @@
+package Database;
+
+public class Controller implements I_Bapers {
+
+	public void main() {
+		// TODO - implement Controller.main
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param data
+	 */
+	public String identifyCustomer(String data) {
+		// TODO - implement Controller.identifyCustomer
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param customerData
+	 */
+	public int createNewCustomer(String customerData) {
+		// TODO - implement Controller.createNewCustomer
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param jobID
+	 */
+	public String verifyJob(int jobID) {
+		// TODO - implement Controller.verifyJob
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param jobID
+	 */
+	public String retrieveJobStatus(int jobID) {
+		// TODO - implement Controller.retrieveJobStatus
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param jobData
+	 */
+	public void addJob(String jobData) {
+		// TODO - implement Controller.addJob
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param jobID
+	 * @param jobData
+	 */
+	public boolean saveJob(int jobID, String jobData) {
+		// TODO - implement Controller.saveJob
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param jobID
+	 * @param priority
+	 */
+	public void updateStatus(int jobID, String priority) {
+		// TODO - implement Controller.updateStatus
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param paymentData
+	 */
+	public boolean MakePayment(String paymentData) {
+		// TODO - implement Controller.MakePayment
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param type
+	 */
+	public void chooseReportType(String type) {
+		// TODO - implement Controller.chooseReportType
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param reportData
+	 */
+	public String generateReport(String reportData) {
+		// TODO - implement Controller.generateReport
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param task_data
+	 */
+	public void addTask(String task_data) {
+		// TODO - implement Controller.addTask
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param taskData
+	 */
+	public void extendTaskList(String taskData) {
+		// TODO - implement Controller.extendTaskList
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param existing_task_ID
+	 */
+	public void removeExistingTask(int existing_task_ID) {
+		// TODO - implement Controller.removeExistingTask
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param existing_task_ID
+	 */
+	public void updateExistingTask(int existing_task_ID) {
+		// TODO - implement Controller.updateExistingTask
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param customerData
+	 */
+	public void updateCustomerDetails(String customerData) {
+		// TODO - implement Controller.updateCustomerDetails
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param task_status
+	 * @param location
+	 */
+	public void updateTaskInfo(String task_status, String location) {
+		// TODO - implement Controller.updateTaskInfo
+		throw new UnsupportedOperationException();
+	}
+
+	public void backup() {
+		// TODO - implement Controller.backup
+		throw new UnsupportedOperationException();
+	}
+
+	public void restore() {
+		// TODO - implement Controller.restore
+		throw new UnsupportedOperationException();
+	}
+
+	public Controller() {
+		// TODO - implement Controller.Controller
+		throw new UnsupportedOperationException();
+	}
+
+}
