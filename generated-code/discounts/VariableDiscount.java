dfccc8293c26d21ef1f38790e81cc9e7a2a7dc29
mk7531 <61507956+mk7531@users.noreply.github.com>
2021-03-01 22:41:06 +0000

    discount package

---
+++ generated-code/discounts/VariableDiscount.java
@@ -0,0 +1,15 @@
+package Discounts;
+
+public class VariableDiscount extends Discount {
+
+	public double calculatePrice() {
+		// TODO - implement VariableDiscount.calculatePrice
+		throw new UnsupportedOperationException();
+	}
+
+	public VariableDiscount() {
+		// TODO - implement VariableDiscount.VariableDiscount
+		throw new UnsupportedOperationException();
+	}
+
+}
