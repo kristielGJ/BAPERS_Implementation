0d51070c667a89578e32a12c6e6900e5535a417e
mk7531 <kaurmanpreet7531@gmail.com>
2021-03-29 15:41:43 +0100

    updated GUI functionality

--- src/model/database/Controller.java
+++ src/model/database/Controller.java
@@ -1,36 +1,44 @@
 package model.database;
 
-import model.admin.alert.Alert;
-//import model.customers.CustomersList;
 import model.jobs.card_payment.transaction.Card_payment_Transaction;
 import model.jobs.card_payment.transaction.I_Card_payment_Transaction;
-import model.jobs.cash_payment.transaction.Cash_payment_Transaction;
-import model.jobs.cash_payment.transaction.I_Cash_payment_Transaction;
+import model.jobs.existing_tasks.ExistingTasks;
 import model.jobs.existing_tasks.transaction.ExistingTasks_Transaction;
 import model.jobs.existing_tasks.transaction.I_ExistingTasks_Transaction;
+import model.jobs.job.Job;
 import model.jobs.job.transaction.I_Job_Transaction;
 import model.jobs.job.transaction.Job_Transaction;
 import model.jobs.payment.transaction.I_Payment_Transaction;
 import model.jobs.payment.transaction.Payment_Transaction;
+import model.jobs.task.Task_List;
 import model.jobs.task.transaction.I_Task_Transaction;
 import model.jobs.task.transaction.Task_Transaction;
+import reports.individual_performance_report.IndividualPerformanceReport;
 import reports.individual_performance_report.transaction.I_IndividualPerformanceReport_Transaction;
 import reports.individual_performance_report.transaction.IndividualPerformanceReport_Transaction;
+import reports.job_performance_report.JobPerformanceReport;
 import reports.job_performance_report.transaction.I_JobPerformanceReport_Transaction;
 import reports.job_performance_report.transaction.JobPerformanceReport_Transaction;
+import reports.summary_performance_report.SummaryPerformanceReport;
 import reports.summary_performance_report.transaction.I_SummaryPerformanceReport_Transaction;
 import reports.summary_performance_report.transaction.SummaryPerformanceReport_Transaction;
-
-import javax.swing.text.html.CSS;
-import java.sql.ResultSet;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Controller implements I_Bapers {
 
 	private static final DB_Connection mainConn = new DB_Connection();
+	private I_Job_Transaction job = new Job_Transaction(mainConn);
+	private I_Task_Transaction task = new Task_Transaction(mainConn);
+	private I_ExistingTasks_Transaction existingTask = new ExistingTasks_Transaction(mainConn);
+	private I_Payment_Transaction payment = new Payment_Transaction(mainConn);
+	private I_Card_payment_Transaction card_payment = new Card_payment_Transaction(mainConn);
+	private I_IndividualPerformanceReport_Transaction individualPerformanceReport = new IndividualPerformanceReport_Transaction(mainConn);
+	private I_JobPerformanceReport_Transaction jobPerformanceReport = new JobPerformanceReport_Transaction(mainConn);
+	private I_SummaryPerformanceReport_Transaction summaryPerformanceReport = new SummaryPerformanceReport_Transaction(mainConn);
 
 	public void main() {
 
@@ -43,125 +51,11 @@ public class Controller implements I_Bapers {
 		throw new UnsupportedOperationException();
 	}
 
-
 	/**
 	 *
 	 * @param customerData
 	 */
 	public void createNewCustomer(String[] customerData) throws SQLException {
-	}
-
-	/**
-	 *
-	 * @param jobID
-	 */
-	public String verifyJob(int jobID) {
-		// TODO - implement Controller.verifyJob
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param jobID
-	 */
-	public String retrieveJobStatus(int jobID) {
-		// TODO - implement Controller.retrieveJobStatus
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param jobData
-	 */
-	public void addJob(String jobData) {
-
-	}
-	/**
-	 *
-	 * @param jobID
-	 * @param jobData
-	 */
-	public boolean saveJob(int jobID, String jobData) {
-
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param jobID
-	 * @param priority
-	 */
-	public void updateStatus(int jobID, String priority) {
-		// TODO - implement Controller.updateStatus
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean MakePayment(String paymentData) {
-		return false;
-	}
-
-	/**
-	 *
-	 * @param paymentData
-	 */
-	public boolean makePayment(String paymentData) {
-		// TODO - implement Controller.MakePayment
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param type
-	 */
-	public void chooseReportType(String type) {
-		// TODO - implement Controller.chooseReportType
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param reportData
-	 */
-	public String generateReport(String reportData) {
-		// TODO - implement Controller.generateReport
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param task_data
-	 */
-	public void addTask(String task_data) {
-		// TODO - implement Controller.addTask
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param taskData
-	 */
-	public void extendTaskList(String taskData) {
-		// TODO - implement Controller.extendTaskList
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param existing_task_ID
-	 */
-	public void removeExistingTask(int existing_task_ID) {
-		// TODO - implement Controller.removeExistingTask
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param existing_task_ID
-	 */
-	public void updateExistingTask(int existing_task_ID) {
-		// TODO - implement Controller.updateExistingTask
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -177,16 +71,6 @@ public class Controller implements I_Bapers {
 		//cL.updateCustomer(customerData);
 	}
 
-	/**
-	 *
-	 * @param task_status
-	 * @param location
-	 */
-	public void updateTaskInfo(String task_status, String location) {
-		// TODO - implement Controller.updateTaskInfo
-		throw new UnsupportedOperationException();
-	}
-
 	public void backup() {
 		// TODO - implement Controller.backup
 		throw new UnsupportedOperationException();
@@ -197,118 +81,99 @@ public class Controller implements I_Bapers {
 		throw new UnsupportedOperationException();
 	}
 
-	I_Job_Transaction job = new Job_Transaction(mainConn);
-
+	//creates a new job
 	public void createJob(int job_ID, String job_desc, String priority, String job_status, int time, String special_instructions, int customer_account_no){
 		job.addJob(job_ID, job_desc, priority, job_status, time, special_instructions, customer_account_no);
 	}
 
-	public String[] retrieveJob(int job_ID){
-		return job.retrieveJob(job_ID);
-	}
-
+	//updates the job status
 	public void updateJobStatus(int job_ID, String job_status){
 		job.updateJobStatus(job_ID, job_status);
 	}
 
-	public void updatePriority(int job_ID, String priority){
-		job.updatePriority(job_ID, priority);
+	//returns a list of jobs which are not completed
+	public ArrayList<Job> getActiveJobs(){
+		return job.getActiveJobs();
 	}
 
-	public void addPaymentDeadline(int job_ID, LocalDateTime payment_deadline){
-		job.addPaymentDeadline(job_ID, payment_deadline);
+	//returns a list of jobs(completed or all)
+	public ArrayList<Job> getJobs(int customer_id, String type){
+		return job.getJobs(customer_id, type);
 	}
 
-	I_Task_Transaction task = new Task_Transaction(mainConn);
-
-	public void createTask(int task_ID, String existing_task, int Job_ID, String technician, String task_status){
-		task.addTask(task_ID, existing_task, Job_ID, technician, task_status);
+	//creates a new task
+	public void createTask(String existing_task, int Job_ID, String technician, String task_status){
+		task.addTask(existing_task, Job_ID, technician, task_status);
 	}
 
-	public String[] retrieveTasks(int task_ID){
-		return task.retrieveTasks(task_ID);
-	}
-
+	// returns a list of existing task names
 	public String[] existingTasks(){
 		return task.retrieveExistingTasks();
 	}
 
+	//returns a list of technician names
 	public String[] retrieveTechnicians(){
 		return task.retrieveTechnicians();
 	}
 
+	//returns a list of task associated with a job
+	public ArrayList<Task_List> getAllTasks(int job_ID){
+		return task.getAllTasks(job_ID);
+	}
+
+	//update the task status
 	public void updateTaskStatus(int Task_ID, String Task_status){
 		task.updateTaskStatus(Task_ID, Task_status);
 	}
 
-	I_ExistingTasks_Transaction existingTask = new ExistingTasks_Transaction(mainConn);
-
-	public void createExistingTask(int exg_Task_ID, String task_description, double task_price, int task_duration, String department_name){
-		existingTask.extendTaskList(exg_Task_ID, task_description, task_price, task_duration, department_name);
+	//creates a new existing task
+	public void createExistingTask(String task_description, double task_price, int task_duration, String department_name){
+		existingTask.extendTaskList(task_description, task_price, task_duration, department_name);
 	}
 
-	public String[] retrieveExistingTask(int existing_task_ID){
-		return existingTask.retrieveExistingTask(existing_task_ID);
+	//returns a list of existing task with details
+	public ArrayList<ExistingTasks> getExistingTasks(){
+		return existingTask.getExistingTasks();
 	}
 
+	//deletes a existing task
 	public void deleteExistingTask(int existing_task_ID){
 		existingTask.removeExistingTask(existing_task_ID);
 	}
 
+	//updates a existing task
 	public void updateExistingTask(int existing_task_ID, String task_name, Double task_price, int task_duration, String department_name){
 		existingTask.updateExistingTask(existing_task_ID, task_name, task_price, task_duration, department_name);
 	}
 
-	I_Payment_Transaction payment = new Payment_Transaction(mainConn);
-
-	public void createPayment(int payment_ID, String payment_type, Double payment_amount, int customer_ID){
-		payment.makePayment(payment_ID, payment_amount, payment_type, customer_ID);
+	//creates a new payment
+	public int createPayment(String payment_type, Double payment_amount, Date date, int job_ID){
+		return payment.makePayment(payment_amount, payment_type, date, job_ID);
 	}
 
+	//generates a invoice
 	public String[] generateInvoice(int payment_ID){
 		return payment.generateInvoice(payment_ID);
 	}
 
-	I_Cash_payment_Transaction cash_payment = new Cash_payment_Transaction(mainConn);
-
-	public void createCash_payment(int payment_ID, double payment_amount, int customer_ID){
-		cash_payment.makeCash_payment(payment_ID, payment_amount, customer_ID);
+	//creates a new card payment
+	public  void createCard_payment(int payment_id, String card_type, String expiry_date, String last_digits, double payment_amount, Date date, int job_ID){
+		card_payment.makeCard_payment(payment_id, payment_amount, card_type, expiry_date, last_digits, date, job_ID);
 	}
 
-	I_Card_payment_Transaction card_payment = new Card_payment_Transaction(mainConn);
-
-	public  void createCard_payment(int card_ID, int payment_ID, String card_type, String expiry_date, String last_digits, double payment_amount, int customer_ID){
-		card_payment.makeCard_payment(card_ID, payment_ID, payment_amount, card_type, expiry_date, last_digits, customer_ID);
+	//generates job sheet
+	public ArrayList<JobPerformanceReport> generateJobSheet(LocalDate from_date, LocalDate to_date, int customer_id){
+		return jobPerformanceReport.generateJobSheet(from_date, to_date, customer_id);
 	}
 
-	I_IndividualPerformanceReport_Transaction individualPerformanceReport = new IndividualPerformanceReport_Transaction(mainConn);
-
-	public HashMap<String, HashMap<Integer, String[][]>> generateIndividualPerformanceReport(LocalDate from_date, LocalDate to_date){
-		return individualPerformanceReport.generateIndividualPerformanceReport(from_date, to_date);
+	//generate individual performance report
+	public ArrayList<IndividualPerformanceReport> generateIndividualPerformance_Report(LocalDate from_date, LocalDate to_date){
+		return individualPerformanceReport.generateIndividualPerformance_Report(from_date, to_date);
 	}
 
-	I_JobPerformanceReport_Transaction jobPerformanceReport = new JobPerformanceReport_Transaction(mainConn);
-
-	public HashMap<Integer, HashMap<Integer, String[][]>> generateJobPerformanceReport(LocalDate from_date, LocalDate to_date, int customer_acc_no){
-		return jobPerformanceReport.generateJobPerformanceReport(from_date, to_date, customer_acc_no);
-	}
-
-	I_SummaryPerformanceReport_Transaction summaryPerformanceReport = new SummaryPerformanceReport_Transaction(mainConn);
-
-	public HashMap<String, String[]> generateCopy_room_performance(LocalDate from_date, LocalDate to_date){
-		return summaryPerformanceReport.copy_room_performance(from_date, to_date);
-	}
-
-	public HashMap<String, String[]> generateDevelopment_performance(LocalDate from_date, LocalDate to_date){
-		return summaryPerformanceReport.development_performance(from_date, to_date);
-	}
-
-	public HashMap<String, String[]> generateFinishing_performance(LocalDate from_date, LocalDate to_date){
-		return summaryPerformanceReport.finishing_performance(from_date, to_date);
-	}
-
-	public HashMap<String, String[]> generatePacking_performance(LocalDate from_date, LocalDate to_date){
-		return summaryPerformanceReport.packing_performance(from_date, to_date);
+	//generated summary performance report
+	public ArrayList<SummaryPerformanceReport> generateSummaryPerformanceReport(LocalDate from_date, LocalDate to_date, LocalTime from_time, LocalTime to_time){
+		return summaryPerformanceReport.generateSummaryPerformanceReport(from_date, to_date, from_time, to_time);
 	}
 
 	public Controller() {
